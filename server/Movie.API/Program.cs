94b5c2676e6346f205b1a1141dfef0b199cb048c
Quang Khuat <kdquang123@gmail.com>
2025-04-29 21:57:04 +0700

    add swagger

--- server/Movie.API/Program.cs
+++ server/Movie.API/Program.cs
@@ -54,12 +54,17 @@ builder.Services.AddCors(options =>
 });
 
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapControllers();
